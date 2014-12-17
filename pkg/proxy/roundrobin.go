4ace825d677d9b431dbc9943af01ff06fe51fa7e
Mike Foley <mfoley@manh.com>
2014-12-17 10:03:42 -0500

    Sticky session implementation and tests

--- pkg/proxy/roundrobin.go
+++ pkg/proxy/roundrobin.go
@@ -22,6 +22,7 @@ import (
 	"reflect"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
 	"github.com/golang/glog"
@@ -32,27 +33,63 @@ var (
 	ErrMissingEndpoints    = errors.New("missing endpoints")
 )
 
+type sessionAffinityDetail struct {
+	ipAddress     string
+	sessionCookie string
+	endpoint      string
+	lastUsedDTTM  time.Time
+}
+
+type serviceDetail struct {
+	name                string
+	sessionAffinityType api.AffinityType
+	sessionAffinityMap  map[string]sessionAffinityDetail
+}
+
 // LoadBalancerRR is a round-robin load balancer.
 type LoadBalancerRR struct {
-	lock         sync.RWMutex
-	endpointsMap map[string][]string
-	rrIndex      map[string]int
+	lock          sync.RWMutex
+	endpointsMap  map[string][]string
+	rrIndex       map[string]int
+	serviceDtlMap map[string]serviceDetail
+}
+
+func newServiceDetail(service string, sessionAffinityType api.AffinityType) *serviceDetail {
+	return &serviceDetail{
+		name:                service,
+		sessionAffinityType: sessionAffinityType,
+		sessionAffinityMap:  make(map[string]sessionAffinityDetail),
+	}
 }
 
 // NewLoadBalancerRR returns a new LoadBalancerRR.
 func NewLoadBalancerRR() *LoadBalancerRR {
 	return &LoadBalancerRR{
-		endpointsMap: make(map[string][]string),
-		rrIndex:      make(map[string]int),
+		endpointsMap:  make(map[string][]string),
+		rrIndex:       make(map[string]int),
+		serviceDtlMap: make(map[string]serviceDetail),
 	}
+}
+
+func (lb *LoadBalancerRR) NewService(service string, sessionAffinityType api.AffinityType) error {
+	if _, exists := lb.serviceDtlMap[service]; !exists {
+		lb.serviceDtlMap[service] = *newServiceDetail(service, sessionAffinityType)
+		glog.V(4).Infof("NewService.  Service does not exist.  So I created it: %+v", lb.serviceDtlMap[service])
+	}
+	return nil
 }
 
 // NextEndpoint returns a service endpoint.
 // The service endpoint is chosen using the round-robin algorithm.
 func (lb *LoadBalancerRR) NextEndpoint(service string, srcAddr net.Addr) (string, error) {
+	var ipaddr string
+	glog.V(4).Infof("NextEndpoint.  service: %s.  srcAddr: %+v. Endpoints: %+v", service, srcAddr, lb.endpointsMap)
+
 	lb.lock.RLock()
-	endpoints, exists := lb.endpointsMap[service]
+	serviceDtls, exists := lb.serviceDtlMap[service]
+	endpoints, _ := lb.endpointsMap[service]
 	index := lb.rrIndex[service]
+
 	lb.lock.RUnlock()
 	if !exists {
 		return "", ErrMissingServiceEntry
@@ -60,10 +97,35 @@ func (lb *LoadBalancerRR) NextEndpoint(service string, srcAddr net.Addr) (string
 	if len(endpoints) == 0 {
 		return "", ErrMissingEndpoints
 	}
+	if serviceDtls.sessionAffinityType != api.AffinityTypeNone {
+		if _, _, err := net.SplitHostPort(srcAddr.String()); err == nil {
+			ipaddr, _, _ = net.SplitHostPort(srcAddr.String())
+		}
+		sessionAffinity, exists := serviceDtls.sessionAffinityMap[ipaddr]
+		glog.V(4).Infof("NextEndpoint.  Key: %s. sessionAffinity: %+v\n", ipaddr, sessionAffinity)
+		if exists && time.Now().Sub(sessionAffinity.lastUsedDTTM).Minutes() < 30 {
+			endpoint := sessionAffinity.endpoint
+			sessionAffinity.lastUsedDTTM = time.Now()
+			glog.V(4).Infof("NextEndpoint.  Key: %s. sessionAffinity: %+v", ipaddr, sessionAffinity)
+			return endpoint, nil
+		}
+	}
 	endpoint := endpoints[index]
 	lb.lock.Lock()
 	lb.rrIndex[service] = (index + 1) % len(endpoints)
+
+	if serviceDtls.sessionAffinityType != api.AffinityTypeNone {
+		affinity, _ := lb.serviceDtlMap[service].sessionAffinityMap[ipaddr]
+		affinity.lastUsedDTTM = time.Now()
+		affinity.endpoint = endpoint
+		affinity.ipAddress = ipaddr
+
+		lb.serviceDtlMap[service].sessionAffinityMap[ipaddr] = affinity
+		glog.V(4).Infof("NextEndpoint. New Affinity key %s: %+v", ipaddr, lb.serviceDtlMap[service].sessionAffinityMap[ipaddr])
+	}
+
 	lb.lock.Unlock()
+	glog.V(4).Infof("NextEndpoint 16. Service Detail Map for %s: %+v", service, lb.serviceDtlMap[service])
 	return endpoint, nil
 }
 
@@ -89,6 +151,31 @@ func filterValidEndpoints(endpoints []string) []string {
 	return result
 }
 
+func removeSessionAffinityByEndpoint(lb *LoadBalancerRR, service string, endpoint string) {
+	for _, seshAffinDtl := range lb.serviceDtlMap[service].sessionAffinityMap {
+		if seshAffinDtl.endpoint == endpoint {
+			delete(lb.serviceDtlMap[service].sessionAffinityMap, seshAffinDtl.ipAddress)
+		}
+	}
+}
+
+func updateServiceDetailMap(lb *LoadBalancerRR, service string, validEndpoints []string) {
+	m := map[string]int{}
+	for _, s1Val := range validEndpoints {
+		m[s1Val] = 1
+	}
+	for _, s2Val := range lb.endpointsMap[service] {
+		m[s2Val] = m[s2Val] + 1
+	}
+	for mKey, mVal := range m {
+		if mVal == 1 {
+			glog.V(3).Infof("Delete endpoint %s for service: %s", mKey, service)
+			removeSessionAffinityByEndpoint(lb, service, mKey)
+			delete(lb.serviceDtlMap[service].sessionAffinityMap, mKey)
+		}
+	}
+}
+
 // OnUpdate manages the registered service endpoints.
 // Registered endpoints are updated if found in the update set or
 // unregistered if missing from the update set.
@@ -102,7 +189,10 @@ func (lb *LoadBalancerRR) OnUpdate(endpoints []api.Endpoints) {
 		validEndpoints := filterValidEndpoints(endpoint.Endpoints)
 		if !exists || !reflect.DeepEqual(existingEndpoints, validEndpoints) {
 			glog.V(3).Infof("LoadBalancerRR: Setting endpoints for %s to %+v", endpoint.Name, endpoint.Endpoints)
+			updateServiceDetailMap(lb, endpoint.Name, validEndpoints)
+			lb.NewService(endpoint.Name, api.AffinityTypeNone) //This is just a catch all in case any existing flows did not create the new service before getting to this point.
 			lb.endpointsMap[endpoint.Name] = validEndpoints
+
 			// Reset the round-robin index.
 			lb.rrIndex[endpoint.Name] = 0
 		}
@@ -113,6 +203,7 @@ func (lb *LoadBalancerRR) OnUpdate(endpoints []api.Endpoints) {
 		if _, exists := registeredEndpoints[k]; !exists {
 			glog.V(3).Infof("LoadBalancerRR: Removing endpoints for %s -> %+v", k, v)
 			delete(lb.endpointsMap, k)
+			delete(lb.serviceDtlMap, k)
 		}
 	}
 }
